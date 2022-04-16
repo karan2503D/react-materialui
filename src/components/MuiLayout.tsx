3f8f5ffc92cc495a287abc058d60607d04421c5e
Kdodeja2503 <96384940+Kdodeja2503@users.noreply.github.com>
2022-04-16 22:23:42 +0530

    Stack Layout

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Box } from '@mui/material' ;
+import { Box, Stack, Divider } from '@mui/material' ;
 
 function MuiLayout() {
   return (
     //   BOX IS USED TO WRAP SOME TAGS IN IT , IT WORKS SAME AS DIV
     // SPAN WILL CONVERT IT INTO SPAN TAG
-      <>
+    // SPACING IN MULTIPLED BY 8 
+      <Stack sx={{ border: '1px solid' }} direction='row' spacing={2} divider={<Divider orientation='vertical' flexItem />}>
           <Box component='span' sx={{
               backgroundColor: 'primary.main',
               color: 'white',
@@ -21,7 +22,9 @@ function MuiLayout() {
           <Box display='flex' height='100px' width='100px' bgcolor='success.light' p={2}>
 
           </Box>
-      </>
+          {/* STACK COMPONENT IS USED TO MANAGE LAYOUT IN 1 DIMENSION I.E EITHER IN HORIZONTAL OR VERTICAL */}
+
+      </Stack>
   )
 }
 
