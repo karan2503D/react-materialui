e26a0b43a44f070f73720ebf50f0bd3c3847363c
Kdodeja2503 <96384940+Kdodeja2503@users.noreply.github.com>
2022-04-16 14:59:06 +0530

    Radio Group and Radio

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import MuiButtons from './components/MuiButtons';
+import MuiRadioButton from './components/MuiRadioButton';
 import MuiSelect from './components/MuiSelect';
 import MuiTextfield from './components/MuiTextField';
 import { MuiTypography } from './components/MuiTypography';
@@ -11,6 +12,7 @@ function App() {
       <MuiButtons />
       <MuiTextfield />
       <MuiSelect />
+      <MuiRadioButton/>
     </div>
   );
 }
